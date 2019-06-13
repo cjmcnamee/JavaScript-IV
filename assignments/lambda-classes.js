fd8849ee9e7afb61ecfda791641aa0c3938b9005
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-06-13 15:45:44 -0400

    made instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,3 +9,18 @@ class Person {
     console.log(`Hello, my name is ${this.name}, I am from ${this.location}`);
   }
 }
+
+class Instructor extends Person {
+  constructor(instructorInfo) {
+    super(instructor);
+    this.specialty = instructor.specialty;
+    this.favLanguage = instructor.favLanguage;
+    this.catchPhrase = instructor.catchPhrase;
+  }
+  demo(subject) {
+    console.log(`Today we are learning about {subject}`)
+  }
+  grade() {
+    console.log(`{student} receives perfect score on {subject}`)
+  }
+}
