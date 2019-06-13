6851014bce992fb1460a4f0ba1f53b74a4791771
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-06-13 16:50:20 -0400

    made pm objects (again, so sorry)

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -33,13 +33,13 @@ class Student extends Person {   // Child Class
     this.favSubjects = studentInfo.favSubjects;
   }
   listsSubjects() {
-    console.log(this.favoriteSubjects);
+    console.log(this.favSubjects);
   }
   PRAssignment(subject) {
-    console.log(`${student.name} has submitted a PR for ${subject}`);
+    console.log(`${this.name} has submitted a PR for ${subject}`);
   }
   sprintChallenge(subject) {
-    console.log(`${student.name} has begun spring challenge on ${subject}`);
+    console.log(`${this.name} has begun sprint challenge on ${subject}`);
   }
 }
 
@@ -59,7 +59,7 @@ class ProjectManagers extends Instructor {   // Grandchild Class
 }
 
 const steve = new Instructor({
-  name: 'Steve',
+  name: 'Minecraft Steve',
   location: 'The Mine',
   age: '???',
   favLanguage: 'Creeper',
@@ -70,7 +70,7 @@ const steve = new Instructor({
 const miku = new Instructor({
   name: 'Hatsune Miku',
   location: 'Japan',
-  age: '16',
+  age: 16,
   favLanguage: 'Japanese',
   specialty: 'Autotune',
   catchPhrase: `"When you order Domino's, Have Some Fun With Miku!"`
@@ -80,9 +80,40 @@ const naruto = new Student ({
   name: 'Uzumaki Naruto',
   location: 'Konoha Village',
   age: 17,
+  previousBackground: 'Ninja',
+  className: 'Team 7',
+  favSubjects: ['Believing', 'Chakra Control', 'Shadow Clone Jutsu']
+})
+
+const connor = new Student ({
+  name: 'Connor McNamee',
+  location: 'Boston',
+  age: 20,
+  previousBackground: 'Fry cook',
+  className: 'Black Mage',
+  favSubjects: ['Art', 'Math', 'HTML']
+})
+
+const pikachu = new ProjectManager ({
+  name: 'Pikachu',
+  location: 'Kanto',
+  age: 1,
+  favLanguage: 'Pika pika',
+  specialty: 'Thunderbolt',
+  catchPhrase: `"Pika pika"`,
+  gradClassName: 'Kanto School for Liberal Arts (working title)',
+  favInstructor: 'Ash Ketchum'
+})
+
+const cinnamon = new ProjectManage ({
+  name: 'Cinnamon Roll',
+  location: 'Japan',
+  age: '4.5 billion',
   favLanguage: 'Japanese',
-  specialty: 'Shadow Clone Jutsu',
-  catchPhrase: '"Im gonna be hokage!!!"'
+  specialty: 'Being cute',
+  catchPhrase: `"o u o"`,
+  gradClassName: 'Sanrio University',
+  favInstructor: 'Hello Kitty'
 })
 
 console.log(steve.name);  // Instructor Steve console.logs
@@ -104,3 +135,23 @@ console.log(miku.catchPhrase);
 miku.speak();
 miku.demo('Vocaloid');
 miku.grade('Vocaloid');
+
+console.log(naruto.name);  // Student Naruto console.logs
+console.log(naruto.location);
+console.log(naruto.age);
+console.log(naruto.previousBackground);
+console.log(naruto.className);
+naruto.speak();
+naruto.listsSubjects();
+naruto.PRAssignment('believeing');
+naruto.sprintChallenge('believing')
+
+console.log(connor.name);  // Student Connor console.logs
+console.log(connor.location);
+console.log(connor.age);
+console.log(connor.previousBackground);
+console.log(connor.className);
+connor.speak();
+connor.listsSubjects();
+connor.PRAssignment('JavaScript IV');
+connor.sprintChallenge('Intro to JavaScript')
