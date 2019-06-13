2b0e5d04fdfe28570579cf266f634dbb16f578fb
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-06-13 16:25:06 -0400

    made Instructor objects (I'm so sorry)

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -20,7 +20,7 @@ class Instructor extends Person {    // Child Class
   demo(subject) {
     console.log(`Today we are learning about ${subject}`);
   }
-  grade(student, subject) {
+  grade(subject) {
     console.log(`${this.name} receives perfect score on ${subject}`);
   }
 }
@@ -57,3 +57,50 @@ class ProjectManagers extends Instructor {   // Grandchild Class
     console.log(`${this.name} debugs ${student.name}'s code on ${subject}'`);
   }
 }
+
+const steve = new Instructor({
+  name: 'Steve',
+  location: 'The Mine',
+  age: '???',
+  favLanguage: 'Creeper',
+  specialty: 'Mining',
+  catchPhrase: `"Play Minecraft; Mine Diamonds"`
+});
+
+const miku = new Instructor({
+  name: 'Hatsune Miku',
+  location: 'Japan',
+  age: '16',
+  favLanguage: 'Japanese',
+  specialty: 'Autotune',
+  catchPhrase: `"When you order Domino's, Have Some Fun With Miku!"`
+});
+
+const naruto = new Student ({
+  name: 'Uzumaki Naruto',
+  location: 'Konoha Village',
+  age: 17,
+  favLanguage: 'Japanese',
+  specialty: 'Shadow Clone Jutsu',
+  catchPhrase: '"Im gonna be hokage!!!"'
+})
+
+console.log(steve.name);  // Instructor Steve console.logs
+console.log(steve.location);
+console.log(steve.age);
+console.log(steve.favLanguage);
+console.log(steve.specialty);
+console.log(steve.catchPhrase);
+steve.speak();
+steve.demo('Farming');
+steve.grade('Mining');
+
+console.log(miku.name);  // Instructor Hatsune Miku console.logs
+console.log(miku.location);
+console.log(miku.age);
+console.log(miku.favLanguage);
+console.log(miku.specialty);
+console.log(miku.catchPhrase);
+miku.speak();
+miku.demo('Vocaloid');
+miku.grade('Vocaloid');
